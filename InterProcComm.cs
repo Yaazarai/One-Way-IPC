d44f9eacd6683d92cb842d57d01d03bd7d468402
Alexander Griffis <fatalsleep@gmail.com>
2016-04-25 16:03:32 -0700

    Update InterProcComm.cs

--- InterProcComm.cs
+++ InterProcComm.cs
@@ -10,25 +10,30 @@ namespace InterProcessCommunication {
         private static string appGUID = Assembly.GetExecutingAssembly().GetType().GUID.ToString();
         private CancellationTokenSource cancellationSource;
         private Task procCommTask;
-        private bool canListen;
 
-        public delegate void MessageReceivedEventHandler(GrowBuffer message);
+        public delegate void MessageReceivedEventHandler(MemoryBuffer message);
         public event MessageReceivedEventHandler MessageReceived;
 
         public InterProcComm(bool start) {
-            if (start)
+            if(start)
                 Start();
         }
 
         private void Listen() {
             while(!cancellationSource.IsCancellationRequested) {
-                using(NamedPipeServerStream pipeServer = new NamedPipeServerStream(appGUID)) {
+                using(NamedPipeServerStream pipeServer = new NamedPipeServerStream(appGUID, PipeDirection.In, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Byte)) {
                     try {
                         pipeServer.WaitForConnection();
-                        GrowBuffer memory = new GrowBuffer(0);
-                        memory.Grow(pipeServer.InBufferSize);
-                        pipeServer.Read(memory.GetBuffer(), 0, pipeServer.InBufferSize);
-                        MessageReceived?.Invoke(memory);
+
+                        MemoryBuffer mb = new MemoryBuffer();
+                        int data = 0;
+                        while((data = pipeServer.ReadByte()) != -1)
+                            mb.Write((byte)data);
+
+                        mb.Memory.Seek(0, SeekOrigin.Begin);
+                        MessageReceived?.Invoke(mb);
+                        pipeServer.Flush();
+
                         pipeServer.Disconnect();
                     } catch(IOException) {
                         pipeServer.Disconnect();
@@ -42,12 +47,13 @@ namespace InterProcessCommunication {
             cancellationSource = null;
         }
 
-        public static bool Post(GrowBuffer memory) {
-            using(NamedPipeClientStream pipeClient = new NamedPipeClientStream(appGUID)) {
+        public static bool Post(MemoryBuffer memory) {
+            using(NamedPipeClientStream pipeClient = new NamedPipeClientStream(".", appGUID, PipeDirection.Out)) {
                 pipeClient.Connect(50);
 
                 if(pipeClient.IsConnected) {
-                    pipeClient.Write(memory.GetBuffer(), 0, memory.Iterator);
+                    byte[] buffer = memory.Memory.GetBuffer();
+                    pipeClient.Write(buffer, 0, buffer.Length);
                     pipeClient.Flush();
                 }
 
